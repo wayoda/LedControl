be7b3d54503ba5f9e38d8bbd72f6296618e7e306
noasakurajin <noasakurajin@web.de>
2020-03-25 11:06:03 +0100

    more board testing and hwSPI demo

---
+++ examples/LedControllerDemoHwSPI/LedControllerDemoHwSPI.ino
@@ -0,0 +1,117 @@
+#include "LedController.hpp"
+
+#define CS 15
+#define Segments 4
+
+#define delayTime 200 // Delay between Frames
+
+LedController lc = LedController(CS,Segments);  // Pins: DIN,CLK,CS, # of Display connected
+
+std::array<byte,8> rocket= {
+  B00000000,
+  B00001111,
+  B00111110,
+  B11111101,
+  B00111110,
+  B00001111,
+  B00000000,
+  B00000000
+};
+
+auto rocketColumns = lc.makeColumns(rocket);
+
+//sets all rows on all displays to 0
+void switchLED(){
+  static bool LEDON = false;
+  if(LEDON){
+    digitalWrite(13, LOW);
+  }else{
+    digitalWrite(13, HIGH);
+  }
+  LEDON = !LEDON;
+}
+
+void setup(){
+
+  pinMode(13, OUTPUT);
+
+  lc.setIntensity(0);
+    
+}
+
+void loop(){
+  
+    lc.clearMatrix();
+    
+    //Let the rocket fly in
+    for(int i = 0;i < 8*(Segments+1);i++){
+      delay(delayTime);
+
+      //blink led for each iteration
+      switchLED();
+
+      //if rocket not fully inside let it fly in and shift it
+      if(i < 8){
+        lc.moveRight(rocketColumns[i]);   
+      }else{
+        lc.moveRight();
+
+        delay(delayTime);
+        switch(i % 6){
+          case(3):
+          case(4):
+          case(5):
+            lc.moveUp();
+            break;
+
+          case(0):
+          case(1):
+          case(2):
+            lc.moveDown();
+            break;
+
+          default:
+            break;
+        }
+      }
+         
+    }
+
+    delay(delayTime);
+
+    for(int i = 0;i < 8*(Segments+1);i++){
+      delay(delayTime);
+
+      //blink led for each iteration
+      switchLED();
+
+      //if rocket not fully inside let it fly in and shift it
+      if(i < 8){
+        lc.moveLeft(rocketColumns[i]);   
+      }else{
+        lc.moveLeft();
+
+        delay(delayTime);
+        switch(i % 6){
+          case(3):
+          case(4):
+          case(5):
+            lc.moveUp();
+            break;
+
+          case(0):
+          case(1):
+          case(2):
+            lc.moveDown();
+            break;
+
+          default:
+            break;
+        }
+      }
+         
+    }
+
+    delay(delayTime);
+
+}
