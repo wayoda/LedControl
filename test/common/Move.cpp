d00b3c62575d6f43ad19155a415c26821bb6ad5f
noasakurajin <noasakurajin@web.de>
2020-10-07 18:42:25 +0200

    improving code quality of tests

--- test/common/Move.cpp
+++ test/common/Move.cpp
@@ -19,87 +19,83 @@ controller_configuration& get_conf(){
 }
 
 void test_left_right(){
-
-    controller_configuration conf = get_conf();
-    
-    TEST_ASSERT_TRUE(conf.isValid());
-
     try{
+        controller_configuration conf = get_conf();
+        TEST_ASSERT_TRUE(conf.isValid());
+
         LedController control = LedController(conf);
 
         auto in = B00000101;
         control.moveLeft(in);
         TEST_ASSERT_EQUAL(in, control.moveRight(0));
-    }catch(const std::exception& e){
-        const char* msg = e.what();
-        TEST_FAIL_MESSAGE(msg);
     }
+    CATCH_STD
     #ifndef ARDUINO
-    catch(const FakeitException& e){
-        std::cout << e.what() << std::endl;
-        const char* msg = e.what().c_str();
-        TEST_FAIL_MESSAGE(msg);
-    }
+    CATCH_FAKEIT
     #endif
-    
 }
 
 void test_right_left(){
-    controller_configuration conf = get_conf();
-
-    TEST_ASSERT_TRUE(conf.isValid());
-
     try{
+        controller_configuration conf = get_conf();
+        TEST_ASSERT_TRUE(conf.isValid());
+
         LedController control = LedController(conf);
 
         auto in = B00000101;
         control.moveRight(in);
         TEST_ASSERT_EQUAL(in, control.moveLeft(0));
-    }catch(const std::exception& e){
-        const char* msg = e.what();
-        TEST_FAIL_MESSAGE(msg);
     }
+    CATCH_STD
     #ifndef ARDUINO
-    catch(const FakeitException& e){
-        std::cout << e.what() << std::endl;
-        const char* msg = e.what().c_str();
-        TEST_FAIL_MESSAGE(msg);
-    }
+    CATCH_FAKEIT
     #endif
 }
 
 void test_up_down(){
-    controller_configuration conf = get_conf();
-    LedController control = LedController(conf);
+    try{
+        controller_configuration conf = get_conf();
+        LedController control = LedController(conf);
 
-    byte in[4];
-    in[0] = B00000001;
-    in[1] = B00001000;
-    in[2] = B00100000;
-    in[3] = B10000000;
-    control.moveUp(in,nullptr);
-    byte* out = new byte[4];
-    control.moveDown(in,&out);
-    for(unsigned int i = 0; i < 4;i++){
-        TEST_ASSERT_EQUAL(in[i],out[i]);
+        byte in[4];
+        in[0] = B00000001;
+        in[1] = B00001000;
+        in[2] = B00100000;
+        in[3] = B10000000;
+        control.moveUp(in,nullptr);
+        byte* out = new byte[4];
+        control.moveDown(in,&out);
+        for(unsigned int i = 0; i < 4;i++){
+            TEST_ASSERT_EQUAL(in[i],out[i]);
+        }
+        delete out;
     }
-    delete out;
+    CATCH_STD
+    #ifndef ARDUINO
+    CATCH_FAKEIT
+    #endif
 }
 
 void test_down_up(){
-    controller_configuration conf = get_conf();
-    LedController control = LedController(conf);
+    try{
+        controller_configuration conf = get_conf();
+        LedController control = LedController(conf);
 
-    byte in[4];
-    in[0] = B00000001;
-    in[1] = B00001000;
-    in[2] = B00100000;
-    in[3] = B10000000;
-    control.moveDown(in,nullptr);
-    byte* out = new byte[4];
-    control.moveUp(in,&out);
-    for(unsigned int i = 0; i < 4;i++){
-        TEST_ASSERT_EQUAL(in[i],out[i]);
+        byte in[4];
+        in[0] = B00000001;
+        in[1] = B00001000;
+        in[2] = B00100000;
+        in[3] = B10000000;
+        control.moveDown(in,nullptr);
+        byte* out = new byte[4];
+        control.moveUp(in,&out);
+        for(unsigned int i = 0; i < 4;i++){
+            TEST_ASSERT_EQUAL(in[i],out[i]);
+        }
+        delete out;
     }
-    delete out;
+    CATCH_STD
+    #ifndef ARDUINO
+    CATCH_FAKEIT
+    #endif
 }
--- test/common/common.hpp
+++ test/common/common.hpp
@@ -4,6 +4,9 @@
 #include "LedController.hpp"
 #include <unity.h>
 
+#define CATCH_STD catch(const std::exception& e){const char* msg = e.what(); TEST_FAIL_MESSAGE(msg); }
+
 #ifndef ARDUINO
     using namespace fakeit;
+    #define CATCH_FAKEIT catch(const FakeitException& e){std::cout << e.what() << std::endl; const char* msg = e.what().c_str(); TEST_FAIL_MESSAGE(msg);}
 #endif
